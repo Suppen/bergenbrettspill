40d24408bb31e4938d7f086c749426a91d7b2de9
Simen de Lange <simen.de.lange@gmail.com>
2021-09-05 13:26:31 +0200

    Oppdaterte koronaretningslinjer

--- client/src/ui/components/frontpage/Pandemic.jsx
+++ client/src/ui/components/frontpage/Pandemic.jsx
@@ -15,43 +15,17 @@ const Pandemic = () => (
 				<h2>Antismittetiltak</h2>
 				<p>Frem til smittefaren er over blir det dessverre en del endringer. Det viktigste er:</p>
 				<ul>
-					<li>
-						Det er <strong>obligatorisk påmelding</strong>¹ gjennom{" "}
-						<a href="https://www.meetup.com/Bergen-Brettspillklubb/" target="_blank" rel="noopener noreferrer">
-							Meetup
-						</a>{" "}
-						(se også eventlisten til høyre). Personer som ikke har meldt seg på vil bli bortvist
-					</li>
-					<li>Det settes en grense på 16 deltakere</li>
-					<li>Vi har ikke plass til mer enn 4 grupper. Det blir derfor 4 spillere pr. spill</li>
-					<li>
-						Hvilket spill man skal spille og hvem man skal spille med må være avtalt på forhånd. Diskusjonsfeltet på
-						Meetup skal brukes til dette²
-					</li>
-					<li>Én person skal være ansvarlig for hvert spill, og sette opp og pakke det ned ved start og slutt</li>
-					<li>Ansiktsmaske anbefales, men er ikke påkrevd</li>
-				</ul>
-				<p>Andre tiltak:</p>
-				<ul>
-					<li>Det skal til enhver tid være minimum 1 meter mellom alle deltakere</li>
-					<li>Hvert spill skal spilles på minst to bord som er satt sammen, uavhengig av spillets størrelse</li>
-					<li>Ikke ta på andres brikker. Hold deg til dine egne</li>
+					<li>Det settes en grense på ca. 30 deltakere. Nøyaktig grense blir en skjønnsmessig vurdering hver onsdag. Personer som har meldt seg på på Meetup vil bli prioritert dersom grensen nås.</li>
+					<li>Man må registrere seg med navn og telefonnummer når man kommer</li>
+					<li>Det skal til enhver tid holdes minst en meter mellom alle deltakere</li>
+					<li>Munnbind er anbefalt, men ikke påbudt</li>
+					<li>Det anbefales å holde seg til egne brikker, og å ikke ta på andres brikker</li>
 					<li>Det settes ut håndsprit på hvert bord. Bruk dette ofte. Såpe og vann er tilgjengelig på toalettene</li>
-					<li>Ingen spising i lokalet</li>
+					<li>Om man skal spise eller drikke, må man trekke seg litt vekk fra bordet</li>
 					<li>Kjøkkenet skal ikke brukes</li>
-					<li>Personer som har vært i utlandet de siste 14 dager skal være i karantene og får ikke komme</li>
-					<li>Personer i risikogruppen frarådes å delta</li>
-					<li>Personer med samfunnskritiske oppgaver frarådes å delta</li>
-					<li>Personer med symptomer vil bli bortvist</li>
-					<li>Personer som ikke overholder reglene vil bli bortvist</li>
 				</ul>
-				<p>Brettspill er en sosial aktivitet, og gjennomføres på egen risiko</p>
-				<p>
-					¹ Husk å melde avbud om du ikke kan likevel! Meetup har venteliste. Om man setter seg på denne, vil man
-					automatisk få plass om noen melder avbud. Personer på ventelisten vil også prioriteres uken etter. Melder du
-					deg på og ikke dukker opp, må du stå over uken etter
-				</p>
-				<p>² Legg inn spillforslag som kommentar på Meetup. Svar på kommentaren for å melde deg på</p>
+				<p>Følg lokale og nasjonale retningslinjer. Brettspill er en sosial aktivitet, og gjennomføres på egen risiko.</p>
+				<p>Om du får symptomer eller påvist covid-19 innen to uker etter spilling, gi beskjed til styret. Kontaktdetaljer finnes øverst på nettsiden, under "Finn oss på...".</p>
 			</div>
 		</div>
 	</div>
